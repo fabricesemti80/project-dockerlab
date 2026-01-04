3c1787aca5e8ba62df2f2617370341a6f6227955
Fabrice Semti <fabrice@fabricesemti.com>
2026-01-04 22:23:23 +0000

    feat(ansible): add role to initialize docker swarm cluster
    This commit introduces a new Ansible role, `docker_swarm`, to automate the setup of a Docker Swarm cluster. The role handles initializing the swarm on the primary manager, retrieving the necessary join tokens, and joining other manager nodes to the cluster.
    
    To support this, the main `site.yml` playbook has been updated to incorporate the new role, which is controlled by the `install_swarm` variable. Additionally, the `fresh_install` role has been refactored to remove its hard-coded dependencies, allowing them to be managed directly in the main playbook for greater flexibility.
    
    The unused `proxmox` inventory group has also been removed from the Ansible inventory and the Terraform configuration.

--- terraform_infra/main_local.tf
+++ terraform_infra/main_local.tf
@@ -1,7 +1,6 @@
 resource "local_file" "ansible_inventory" {
   filename = "../ansible/inventory/hosts"
   content  = <<-EOT
-
 [proxmox_vms]
 ${module.dkr_srv_1.vm_name} ansible_host=${split("/", module.dkr_srv_1.vm_ipv4_address)[0]}
 ${module.dkr_srv_2.vm_name} ansible_host=${split("/", module.dkr_srv_2.vm_ipv4_address)[0]}
@@ -14,7 +13,6 @@ proxmox_vms
 proxmox_vms
 
 [all:children]
-proxmox
 vms
 EOT
 }
