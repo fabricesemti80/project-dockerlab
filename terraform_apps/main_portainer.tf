b79cf6c3b18ae9989b4921505fcfd444ab71a1ae
Fabrice Semti <fabrice@fabricesemti.com>
2026-01-17 12:46:08 +0000

    feat(otterwiki): add otterwiki stack and data directories
    - Add portainer stack configuration for otterwiki
    - Create cephfs data directories for otterwiki
    - Include new docker/otterwiki directory with stack configuration

--- terraform_apps/main_portainer.tf
+++ terraform_apps/main_portainer.tf
@@ -356,3 +356,30 @@ resource "portainer_stack" "glance" {
     value = var.TZ
   }
 }
+
+resource "portainer_stack" "otterwiki" {
+  name            = "otterwiki"
+  deployment_type = "swarm"
+  method          = "repository"
+  endpoint_id     = 1
+
+  repository_url            = var.REPO_URL
+  repository_reference_name = var.REPO_BRANCH
+  file_path_in_repository   = "docker/otterwiki/otterwiki-stack.yml"
+
+  force_update    = true
+  pull_image      = true
+  prune           = true
+  update_interval = "5m"
+  stack_webhook   = true
+
+  env {
+    name  = "DOMAIN"
+    value = var.DOMAIN
+  }
+
+  env {
+    name  = "TZ"
+    value = var.TZ
+  }
+}
