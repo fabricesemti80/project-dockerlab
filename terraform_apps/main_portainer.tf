af099bab5ad01a0d2889a4a92cb0f746994fb9e7
Fabrice Semti <fabrice@fabricesemti.com>
2026-01-18 14:38:19 +0000

    chore(terraform): remove ghost stack and comment out docmost stack
    - Delete main_ghost.tf file completely
    - Comment out docmost stack configuration in main_portainer.tf
    - Add commented ghost stack configuration to main_portainer.tf for reference

--- terraform_apps/main_portainer.tf
+++ terraform_apps/main_portainer.tf
@@ -261,42 +261,42 @@ resource "portainer_stack" "gatus" {
   }
 }
 
-resource "portainer_stack" "docmost" {
-  name            = "docmost"
-  deployment_type = "swarm"
-  method          = "repository"
-  endpoint_id     = 1
+# resource "portainer_stack" "docmost" {
+#   name            = "docmost"
+#   deployment_type = "swarm"
+#   method          = "repository"
+#   endpoint_id     = 1
 
-  repository_url            = var.REPO_URL
-  repository_reference_name = var.REPO_BRANCH
-  file_path_in_repository   = "docker/docmost/docmost-stack.yml"
+#   repository_url            = var.REPO_URL
+#   repository_reference_name = var.REPO_BRANCH
+#   file_path_in_repository   = "docker/docmost/docmost-stack.yml"
 
-  force_update    = true
-  pull_image      = true
-  prune           = true
-  update_interval = "5m"
-  stack_webhook   = true
+#   force_update    = true
+#   pull_image      = true
+#   prune           = true
+#   update_interval = "5m"
+#   stack_webhook   = true
 
-  env {
-    name  = "DOMAIN"
-    value = var.DOMAIN
-  }
+#   env {
+#     name  = "DOMAIN"
+#     value = var.DOMAIN
+#   }
 
-  env {
-    name  = "TZ"
-    value = var.TZ
-  }
+#   env {
+#     name  = "TZ"
+#     value = var.TZ
+#   }
 
-  env {
-    name  = "DOCMOST_APP_SECRET"
-    value = var.DOCMOST_APP_SECRET
-  }
+#   env {
+#     name  = "DOCMOST_APP_SECRET"
+#     value = var.DOCMOST_APP_SECRET
+#   }
 
-  env {
-    name  = "DOCMOST_POSTGRES_PASSWORD"
-    value = var.DOCMOST_POSTGRES_PASSWORD
-  }
-}
+#   env {
+#     name  = "DOCMOST_POSTGRES_PASSWORD"
+#     value = var.DOCMOST_POSTGRES_PASSWORD
+#   }
+# }
 
 resource "portainer_stack" "maintenance" {
   name            = "maintenance"
@@ -383,3 +383,65 @@ resource "portainer_stack" "otterwiki" {
     value = var.TZ
   }
 }
+
+# resource "portainer_stack" "ghost" {
+#   name            = "ghost"
+#   deployment_type = "swarm"
+#   method          = "repository"
+#   endpoint_id     = 1
+
+#   repository_url            = var.REPO_URL
+#   repository_reference_name = var.REPO_BRANCH
+#   file_path_in_repository   = "docker/ghost/ghost-stack.yml"
+
+#   force_update    = true
+#   pull_image      = true
+#   prune           = true
+#   update_interval = "5m"
+#   stack_webhook   = true
+
+#   env {
+#     name  = "DOMAIN"
+#     value = var.DOMAIN
+#   }
+
+#   env {
+#     name  = "GHOST_DB_PASSWORD"
+#     value = var.GHOST_DB_PASSWORD
+#   }
+
+#   env {
+#     name  = "GHOST_DB_ROOT_PASSWORD"
+#     value = var.GHOST_DB_ROOT_PASSWORD
+#   }
+
+#   env {
+#     name  = "GHOST_MAIL_TRANSPORT"
+#     value = var.GHOST_MAIL_TRANSPORT
+#   }
+
+#   env {
+#     name  = "GHOST_MAIL_HOST"
+#     value = var.GHOST_MAIL_HOST
+#   }
+
+#   env {
+#     name  = "GHOST_MAIL_PORT"
+#     value = var.GHOST_MAIL_PORT
+#   }
+
+#   env {
+#     name  = "GHOST_MAIL_USER"
+#     value = var.GHOST_MAIL_USER
+#   }
+
+#   env {
+#     name  = "GHOST_MAIL_PASSWORD"
+#     value = var.GHOST_MAIL_PASSWORD
+#   }
+
+#   env {
+#     name  = "GHOST_MAIL_FROM"
+#     value = var.GHOST_MAIL_FROM
+#   }
+# }
