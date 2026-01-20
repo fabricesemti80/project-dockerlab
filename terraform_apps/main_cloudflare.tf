c6525f0a41987dd302834edfb8e6548f63340292
Fabrice Semti <fabrice@fabricesemti.com>
2026-01-20 09:45:15 +0000

    Change session duration from 24h to 30d

--- terraform_apps/main_cloudflare.tf
+++ terraform_apps/main_cloudflare.tf
@@ -81,7 +81,7 @@ resource "cloudflare_zero_trust_access_application" "homelab_access" {
   domain     = "*.${var.DOMAIN}"
   type       = "self_hosted"
 
-  session_duration          = "24h"
+  session_duration          = "30d"
   auto_redirect_to_identity = false
 
   policies = [
