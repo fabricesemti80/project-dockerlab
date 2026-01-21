307491302b2f5f8232ddea142f004f529eb273a9
Fabrice Semti <fabrice@fabricesemti.com>
2026-01-21 19:07:22 +0000

    feat(infra): add two docker swarm worker nodes
    Add dkr-wrkr-1 and dkr-wrkr-2 as new worker VMs in the Proxmox infrastructure,
    configured with appropriate networking and added to Ansible inventory groups for
    swarm deployment. This expands the swarm capacity by introducing dedicated worker
    nodes separate from the manager servers.

--- terraform_infra/main_local.tf
+++ terraform_infra/main_local.tf
@@ -5,12 +5,23 @@ resource "local_file" "ansible_inventory" {
 ${module.dkr_srv_1.vm_name} ansible_host=${split("/", module.dkr_srv_1.vm_ipv4_address)[0]}
 ${module.dkr_srv_2.vm_name} ansible_host=${split("/", module.dkr_srv_2.vm_ipv4_address)[0]}
 ${module.dkr_srv_3.vm_name} ansible_host=${split("/", module.dkr_srv_3.vm_ipv4_address)[0]}
+${module.dkr_wrkr_1.vm_name} ansible_host=${split("/", module.dkr_wrkr_1.vm_ipv4_address)[0]}
+${module.dkr_wrkr_2.vm_name} ansible_host=${split("/", module.dkr_wrkr_2.vm_ipv4_address)[0]}
+
+[swarm_servers]
+${module.dkr_srv_1.vm_name}
+${module.dkr_srv_2.vm_name}
+${module.dkr_srv_3.vm_name}
+
+[swarm_workers]
+${module.dkr_wrkr_1.vm_name}
+${module.dkr_wrkr_2.vm_name}
 
 [vms:children]
 proxmox_vms
 
 [swarm_managers:children]
-proxmox_vms
+swarm_servers
 
 [all:children]
 vms
