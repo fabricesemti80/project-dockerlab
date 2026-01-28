58d610a73abaa83b5141cce1eab7037d4d54f97a
Fabrice Semti <fabrice@fabricesemti.com>
2026-01-28 19:05:22 +0000

    feat(media): add jellyseerr media request management
    Add Jellyseerr docker stack for media requests, including Ansible configuration for data directories, Terraform deployment, and new swarm management taskfile. Also improve nzbget healthcheck reliability.

---
+++ terraform_apps/main_portainer_stack_jellyseerr.tf
@@ -0,0 +1,38 @@
+resource "portainer_stack" "jellyseerr" {
+  name            = "jellyseerr"
+  deployment_type = "swarm"
+  method          = "repository"
+  endpoint_id     = data.portainer_environment.local_swarm.id
+
+  repository_url            = var.repo_url
+  repository_reference_name = var.repo_branch
+  file_path_in_repository   = "docker/jellyseerr/jellyseerr-stack.yml"
+
+  force_update    = true
+  pull_image      = true
+  prune           = true
+  update_interval = "5m"
+  stack_webhook   = true
+
+  env {
+    name  = "DOMAIN"
+    value = var.domain
+  }
+
+  env {
+    name  = "TZ"
+    value = var.tz
+  }
+
+  env {
+    name  = "PUID"
+    value = var.media_puid
+  }
+
+  env {
+    name  = "PGID"
+    value = var.media_pgid
+  }
+
+  depends_on = [portainer_stack.traefik]
+}
